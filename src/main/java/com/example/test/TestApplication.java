dfb95edbadca13f0adbbaf516c34cb6827581da0
kuseulki <kuseulki@naver.com>
2024-03-06 03:27:12 +0900

    #3 - application.yml 설정

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class 5TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
